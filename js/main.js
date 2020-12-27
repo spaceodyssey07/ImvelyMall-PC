1967b38182a2570a3c7f09461aff9742132abbd0
spaceodyssey07 <spaceodyssey07@gmail.comm>
2020-12-27 10:47:33 +0900

    2020-12-27(이준재) 1th commit

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 // 메인 네비게이션
-var mainNav = document.querySelector(".main-nav")
+var mainNav = document.querySelector(".main-nav-wrap")
 var hoverGnb = document.querySelector(".header .header-left .gnb")
-var whiteHeader = document.querySelector(".header-wrap")
 var navFlag = false
 hoverGnb.addEventListener("mouseover", () => mainNav.classList.add("on"))
-hoverGnb.addEventListener("mouseover", () => whiteHeader.classList.add("on"))
+
 mainNav.addEventListener("mouseover", () => (navFlag = true))
 mainNav.addEventListener("mouseleave", () => {
   if (navFlag) {
     navFlag = false
     mainNav.classList.remove("on")
-    whiteHeader.classList.remove("on")
   }
 })
 
